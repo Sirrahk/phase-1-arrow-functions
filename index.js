7f2f3e48bf69b86edfe0b12d01ea0e33a0918ffd
Keara Harris <keara.s.harris@gmail.com>
2023-04-07 02:01:02 -0500

    Completed arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    let num1 = 2000;
+    let num2 = 100;
+    return num1/num2;
+}
+
+const square = valueX => {
+    return valueX * valueX;
+}
+
+const add = (valueX, valueY) =>{
+    return valueX + valueY;
+}
